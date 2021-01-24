d6429f5796110712838dac82e3059cf0c38c96e0
kchatterjee26 <76661670+kchatterjee26@users.noreply.github.com>
2021-01-24 13:52:38 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,30 +1,45 @@
-var rect1;
-var rect2;
+var fixedRect, movingRect, rect1, rect2, rect3;
 
 function setup() {
-  createCanvas(800,400);
-  rect1 = createSprite(400, 200, 50, 50);
-  rect1.shapeColor = "red";
-  rect2 = createSprite(250,60,20,70);
-  rect2.shapeColor = "blue";
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  rect1 = createSprite(200,200,50,50);
+  rect1.shapeColor = "yellow";
+  rect1.debug = true;
+
+  rect2 = createSprite(1000, 20, 50, 50);
+  rect2.shapeColor = "green";
+  rect2.debug = true;
+  rect2.velocityY = 2;
+
+  rect3 = createSprite(1000, 600, 80, 30);
+  rect3.shapeColor = "green";
+  rect3.debug = true;
+  rect3.velocityY = -2;
 }
 
 function draw() {
-  background(0,0,0); 
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
 
-  rect2.y = mouseY;
-  rect2.x = mouseX;
-
-  if (rect2.x - rect1.x < rect2.width/2 + rect1.width/2 &&
-    rect1.x - rect2.x < rect2.width/2 + rect1.width/2 && 
-    rect2.y - rect1.y < rect2.height/2 + rect1.height/2 &&
-    rect1.y - rect2.y < rect1.height/2 + rect1.height/2) {
-    rect1.shapeColor = "yellow";
-    rect2.shapeColor = "yellow";
+  if (touching(movingRect, rect1) === true) {
+    movingRect.shapeColor = "red";
+    rect1.shapeColor = "blue";
   }
     else {
-      rect1.shapeColor = "red";
-      rect2.shapeColor = "blue";
+      movingRect.shapeColor = "green";
+      rect1.shapeColor = "yellow";
     }
+
+    bounce(rect2,rect3);
+
   drawSprites();
 }
